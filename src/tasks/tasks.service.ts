1ddace7a0eed7fa080eac08f4b7a061876c6af54
Nguyễn Hải Triều <36495153+Czer0C@users.noreply.github.com>
2020-11-30 19:48:10 +0700

    get all task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { timeStamp } from 'console';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks = ['Drill', 'TSF Training', 'Team Building', 'Suffering'];
+
+  getAllTasks() {
+    return this.tasks.find(i => i.includes("S"));
+  }
+}
