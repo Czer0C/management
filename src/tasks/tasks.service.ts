204da46ffb98f6c5336f77569e23ee1fee227a77
Nguyễn Hải Triều <36495153+Czer0C@users.noreply.github.com>
2020-12-02 18:02:50 +0700

    get task with filtering

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -13,32 +13,11 @@ export class TasksService {
   constructor (
     @InjectRepository(TaskRepository)
     private taskRepository: TaskRepository,
-  ){} 
-  // private tasks: Task[] = [];
-
-  // getAllTasks(): Task[] {
-  //   return this.tasks; 
-  // }
-
-  // getTasksWithFilters(filterDTO: GetTasksFilterDTO): Task[] {
-  //   const { status, search } = filterDTO;
-
-  //   let tasks = this.getAllTasks();
-
-  //   if (status) {
-  //     tasks = tasks.filter(task => task.status === status);
-  //   }
-
-  //   if (search) {
-  //     tasks = tasks.filter(task => 
-  //       task.title.includes(search) || 
-  //       task.description.includes(search),
-  //     );
-  //   }
-
-  //   return tasks;
-  // }
-
+  ) {}
+  
+  async getTasks(filterDTO: GetTasksFilterDTO): Promise<Task[]> {
+    return this.taskRepository.getTasks(filterDTO);
+  }
 
   async getTaskById(id: number): Promise<Task> {
     const task = await this.taskRepository.findOne(id);
@@ -54,33 +33,12 @@ export class TasksService {
     return this.taskRepository.createTask(createTaskDTO);
   }
 
-  // createTask(createTaskDTO: CreateTaskDTO): Task {
-  //   const { title, description } = createTaskDTO;
-
-  //   const task: Task = {
-  //     id: uuidv4(),
-  //     title,
-  //     description,
-  //     status: TaskStatus.OPEN,
-  //   };
-
-  //   this.tasks.push(task);
-
-  //   return task;
-  // }
-
   async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
     const task = await this.getTaskById(id);
     task.status = status;
     await task.save();
     return task
   }
-
-  // updateTask(id: string, status: TaskStatus): Task {
-  //   let task = this.getTaskById(id);
-  //   task.status = status;
-  //   return task;
-  // }
 
   async deleteTask(id: number): Promise<void> {
     const result = await this.taskRepository.delete(id);
