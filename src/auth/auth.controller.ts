9481bdb9507716237c144434b2c704ba9edfd5d0
Nguyễn Hải Triều <36495153+Czer0C@users.noreply.github.com>
2020-12-03 11:43:46 +0700

    get user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,6 @@
-import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthCredentialsDTO } from './DTO/auth-credentials.dto';
 import { AuthService } from './auth.service';
-import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -19,12 +18,5 @@ export class AuthController {
   @Post('/signin')
   signIn(@Body(ValidationPipe) authCredentials: AuthCredentialsDTO): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentials);
-  }
-
-  @Post('/test')
-  @UseGuards(AuthGuard())
-  test(@Req() req) {
-    console.log(req);
-
   }
 }
